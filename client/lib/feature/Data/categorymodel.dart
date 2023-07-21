ba3971386d65b58f35d091bc04fee507c79a135f
leuleyasu <leuleyasu7@gmail.com>
2023-07-21 18:12:40 +0300

    feat: adding static product Slider with Carausel

---
+++ client/lib/feature/Data/categorymodel.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+class CategoryModel extends Equatable {
+
+   final String name;
+   final String imageUrl;
+   CategoryModel({
+    required this.name,
+    required this.imageUrl,
+  });
+  @override
+  List<Object?> get props => [name,imageUrl];
+ static List<CategoryModel> categories = [
+    CategoryModel(
+      name: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1534057308991-b9b3a578f1b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/5lZhD2qQ2SE
+    ),
+    CategoryModel(
+      name: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1502741224143-90386d7f8c82?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80', //https://unsplash.com/photos/m741tj4Cz7M
+    ),
+    CategoryModel(
+      name: 'Water',
+      imageUrl:
+          'https://images.unsplash.com/photo-1559839914-17aae19cec71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/7Zlds3gm7NU
+    ),
+  ];
+}
---
+++ client/lib/feature/Presentation/Screen/Widgets/custombottmbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomBottombar extends StatelessWidget {
+  const CustomBottombar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return     BottomAppBar(
+      color: Colors.black,
+      height: 50,
+      child: Container(
+      decoration: BoxDecoration(
+        color: Colors.black
+      ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(onPressed: (){
+          Navigator.pushNamed(context, '');
+        }, icon: Icon(Icons.home_outlined, color: Colors.amber,))
+       , IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart,color: Colors.amber,))
+        ,IconButton(onPressed: (){}, icon: Icon(Icons.person_outlined,color: Colors.amber,))
+      ],
+    ),
+      ),
+    );
+  }
+}
