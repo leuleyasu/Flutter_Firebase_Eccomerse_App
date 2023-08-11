3f2a0dd645967c7b873431b44b21d29a590f9a60
leuleyasu <leuleyasu7@gmail.com>
2023-08-11 19:24:49 +0300

    feat:adding state managemnt when featching the data from firbasefirestore

--- client/lib/feature/Eccomerse_Mobile_App/Presentation/bloc/product/bloc/product_event.dart
+++ client/lib/feature/Eccomerse_Mobile_App/Presentation/bloc/product/bloc/product_event.dart
@@ -1,4 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'product_bloc.dart';
 
-@immutable
-sealed class ProductEvent {}
+abstract class Productevent extends Equatable {
+  const Productevent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class Loadproduct extends Productevent {}
+
+// ignore: camel_case_types
+class updateproduct extends Productevent {
+  final List<ProductModel> product;
+
+  const updateproduct(this.product);
+
+  @override
+  List<Object> get props => [product];
+}
